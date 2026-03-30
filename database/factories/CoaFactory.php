eefab31fc18f314d1d36ff7baa8e8a4c93d7b7a6
antheasafana <anthea22safana@gmail.com>
2026-03-30 12:18:11 +0700

    first commit

---
+++ database/factories/CoaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Coa;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Coa>
+ */
+class CoaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
